cab5b682f4db79254e3b5bb13ae23ec66c4665e9
Admin <Admin@Admins-MacBook-Pro.local>
2020-03-19 11:06:27 -0500

    Update

--- app/src/main/java/com/example/cse438/cse438_assignment4/MainActivity.kt
+++ app/src/main/java/com/example/cse438/cse438_assignment4/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.cse438.cse438_assignment4
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
