dc666557d4924caff05855fb319e32660e56501a
Admin <Admin@Admins-MacBook-Pro.local>
2020-03-26 19:56:15 -0500

    clean

--- app/src/main/java/com/example/cse438/cse438_assignment4/activities/LoginActivity.kt
+++ app/src/main/java/com/example/cse438/cse438_assignment4/activities/LoginActivity.kt
@@ -2,17 +2,13 @@ package com.example.cse438.cse438_assignment4.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cse438.cse438_assignment4.R
 import com.firebase.ui.auth.AuthUI
-
 import com.firebase.ui.auth.IdpResponse
-import com.firebase.ui.auth.data.model.User
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
